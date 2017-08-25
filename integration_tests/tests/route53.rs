a11eb839307f8cec70276798d6a7f54c56aa8ccb
Matthew Mayer <matthewkmayer@gmail.com>
2017-08-24 18:04:04 -0700

    Integration test for creation of R53 A-record.

--- integration_tests/tests/route53.rs
+++ integration_tests/tests/route53.rs
@@ -3,7 +3,7 @@
 extern crate rusoto_core;
 extern crate rusoto_route53;
 
-use rusoto_route53::{Route53, Route53Client, ListHostedZonesRequest};
+use rusoto_route53::{Route53, Route53Client, ListHostedZonesRequest, ChangeResourceRecordSetsRequest, ChangeBatch, Change, ResourceRecordSet};
 use rusoto_core::{DefaultCredentialsProvider, Region, default_tls_client};
 
 #[test]
@@ -14,4 +14,31 @@ fn should_list_hosted_zones() {
     let request = ListHostedZonesRequest::default();
 
     client.list_hosted_zones(&request).unwrap();
+}
+
+#[test]
+fn should_create_a_record() {
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client =
+        Route53Client::new(default_tls_client().unwrap(), credentials, Region::UsEast1);
+
+    let resource_record_set = ResourceRecordSet {
+        name: "my full qualified domain".to_owned(),
+        type_: "A".to_owned(),
+        ..Default::default()
+    };
+    let change = Change {
+        action: "CREATE".to_owned(),
+        resource_record_set: resource_record_set,
+    };
+    let change_batch = ChangeBatch {
+        changes: vec!(change),
+        ..Default::default()
+    };
+    let request = ChangeResourceRecordSetsRequest {
+        change_batch: change_batch,
+        hosted_zone_id: "my-hosted-zone".to_owned(),
+    };
+
+    client.change_resource_record_sets(&request).expect("Couldn't create resource record set");
 }
